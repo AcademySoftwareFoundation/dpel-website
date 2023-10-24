923e7513176dbeec3f92234913f1606290898ac8
John Loy <john.loy@dreamworks.com>
2023-10-24 09:54:12 -0700

    fix: favicon, breadcrumbs, license links1
    Signed-off-by: John Loy <john.loy@dreamworks.com>

--- src/utils/sortCards.js
+++ src/utils/sortCards.js
@@ -1,10 +1,6 @@
 // Sorts the order of the cards on the home page
 export function sortCards(cards) {
-  let sortedCards = cards.sort(
-    (a, b) => (
-      a.frontmatter.order > b.frontmatter.order) ? 1 
-      : (a.frontmatter.order < b.frontmatter.order) ? -1 
-      : 0 );
-
-  return sortedCards;
+  return cards.sort((a, b) =>
+    a.data.order > b.data.order ? 1 : a.data.order < b.data.order ? -1 : 0,
+  );
 }
